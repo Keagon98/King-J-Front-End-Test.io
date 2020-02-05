09a49beab59afd0590c49e47af41bd5b045721e8
Keagon Brinkhuis <keaggzb2v@gmail.com>
2020-02-05 16:11:52 +0200

    added all fuctionality and animations, added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# King James Digital Webpage design.
+
+> A single page mobile first website that was built with a modern UI design and features some minimal functionality.
+
+## Built using:
+
+>HTML5
+>CSS3/SCSS
+>JavaScript
+
+## Author
+
+Keagon Brinkhuis
+
+To view demo [Click Here](https://keagon98.github.io/King-J-Front-End-Test.io/).
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+
+
+
 window.sr = ScrollReveal();
 sr.reveal('.logo', {
-    duration: 2500,
+    duration: 2000,
     origin: 'left',
     distance: '500px'
 });
 
 sr.reveal('.content', {
     duration: 2000,
-    origin: 'bottom',
-    distance: '200px',
+    origin: 'right',
+    distance: '400px',
     viewFactor: 0.2
 });
 
@@ -26,12 +29,60 @@ sr.reveal('p', {
     viewFactor: 0.2
 });
 
-sr.reveal('.btn', {
+sr.reveal('.image-slider-1', {
     duration: 2000,
-    origin: 'left',
+    origin: 'right',
     distance: '200px',
     viewFactor: 0.2
 });
+
+sr.reveal('.glyph', {
+    duration: 2000,
+    origin: 'top',
+    delay: 500,
+    distance: '200px',
+    viewFactor: 0.2
+});
+
+sr.reveal('.image-slider-2', {
+    duration: 2000,
+    origin: 'left',
+    distance: '400px',
+    viewFactor: 0.2
+});
+
+sr.reveal('.slide-right-data', {
+    duration: 2000,
+    origin: 'right',
+    delay: 500,
+    distance: '400px',
+    viewFactor: 0.2
+});
+
+AOS.init();
+
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
 
 let navWrapper = document.querySelector('.nav-wrapper'),
 navToogler =  document.querySelector('.nav-toogler')
